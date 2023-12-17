2e34e6ccfaa8f87c5994d5e3ee022dee717553ab
RadosavPanic <radosav.panic01@gmail.com>
2023-12-17 19:27:05 +0100

    checking different data types in js

---
+++ Day-2/checking_types.js
@@ -0,0 +1,16 @@
+// Checking data types
+
+console.log(typeof "Radosav"); // string
+console.log(typeof 10, typeof 3.14); // number number
+console.log(typeof true, typeof false); // boolean boolean
+console.log(typeof NaN); // number
+console.log(typeof Infinity); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+console.log(typeof Symbol); // function
+console.log(typeof Object); // function
+console.log(typeof Array); // function
+console.log(typeof String, typeof Number, typeof Boolean); // function
+console.log(typeof []); // object
+console.log(typeof BigInt(10)); // bigint
