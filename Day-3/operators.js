b09cd2ed63da1c2c570a50077db90d5118890e20
RadosavPanic <radosav.panic01@gmail.com>
2023-12-18 01:57:01 +0100

    working with comparison operators

--- Day-3/operators.js
+++ Day-3/operators.js
@@ -35,3 +35,31 @@ const gravity = 9.81;
 let mass = 72;
 const weight = mass * gravity;
 console.log(weight); // 706.32
+
+/* Comparison operators */
+
+console.log(3 > 2, 3 >= 2); // true, true
+console.log(3 < 2, 2 < 3, 2 <= 3); // false, true, true
+console.log(3 == 2, 3 != 2); // false, true
+
+console.log(3 == "3", 3 === "3"); // true (loose equality), false (strict equality, also data type)
+console.log(3 != "3", 3 !== "3"); // false, true
+console.log(3 != 3, 3 !== 3); // false, false
+
+console.log(0 == false); // true
+console.log(0 === false); // false, number != boolean
+
+console.log(0 == "", 0 == " "); // true, true
+console.log(0 === ""); // false, number != string
+
+console.log(1 == true, 1 === true); // true, false (number != boolean)
+
+console.log(undefined == null); // true
+console.log(undefined === null); // false, undefined != object
+
+console.log(NaN == NaN, NaN === NaN, typeof NaN); // false, false, number (usually number==number is true, but NaN is not a number only)
+
+console.log(3 !== "3"); // true
+console.log(3 != "3"); // false
+console.log(3 !== 3); // false
+console.log(3 != 3); // false
