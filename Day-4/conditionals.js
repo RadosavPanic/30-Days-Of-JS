1647d1093f2e92ea121e7d1afdd0b029e22406c5
RadosavPanic <radosav.panic01@gmail.com>
2023-12-23 13:49:27 +0100

    made functions for checking season, working days and student grading

--- Day-4/conditionals.js
+++ Day-4/conditionals.js
@@ -72,3 +72,85 @@ promptNum &&
   promptNum != "NaN" &&
   typeof promptNum === "number" &&
   checkIsNumberEven(promptNum);
+
+// Check season function (Autumn, Winter, Spring, Summer)
+const CheckSeasonOfYear = (monthInput) => {
+  if (monthInput === null || monthInput === undefined) return;
+
+  let month = monthInput.toLowerCase();
+  month = month.replace(month[0], month[0].toUpperCase());
+
+  switch (month) {
+    case "September":
+    case "October":
+    case "November":
+      console.log(`Season for month ${month} is Autumn.`);
+      break;
+    case "December":
+    case "January":
+    case "February":
+      console.log(`Season for month ${month} is Winter.`);
+      break;
+    case "March":
+    case "April":
+    case "May":
+      console.log(`Season for month ${month} is Spring.`);
+      break;
+    case "June":
+    case "July":
+    case "August":
+      console.log(`Season for month ${month} is Summary.`);
+      break;
+    default:
+      console.log("Entered month is not valid.");
+  }
+};
+
+let monthOfYear = prompt("Enter month");
+CheckSeasonOfYear(monthOfYear);
+
+// Check day function (working or weekend)
+const checkIsWorkingOrWeekendDay = (dayInput) => {
+  if (dayInput === null || dayInput === undefined) return;
+
+  let day = dayInput.toLowerCase();
+  day = day.replace(day[0], day[0].toUpperCase());
+
+  switch (day) {
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+      console.log(`${day} is a working day.`);
+      break;
+    case "Saturday":
+    case "Sunday":
+      console.log(`${day} is a weekend day.`);
+      break;
+    default:
+      console.log("Entered day is not valid.");
+  }
+};
+
+let dayInput = prompt("Enter your day:");
+checkIsWorkingOrWeekendDay(dayInput);
+
+// Check student grading function
+const checkStudentGrade = (points) => {
+  let grade;
+  if (points >= 90 && points <= 100) grade = "A";
+  else if (points >= 70 && points <= 89) grade = "B";
+  else if (points >= 60 && points <= 69) grade = "C";
+  else if (points >= 50 && points <= 59) grade = "D";
+  else if (points >= 0 && points <= 49) grade = "F";
+  else console.log("Entered points are out of bounds.");
+
+  grade && console.log(`Your points: ${points} (grade ${grade})`);
+};
+
+let studentPoints = +prompt("Enter student points");
+studentPoints &&
+  studentPoints != "NaN" &&
+  typeof studentPoints === "number" &&
+  checkStudentGrade(studentPoints);
