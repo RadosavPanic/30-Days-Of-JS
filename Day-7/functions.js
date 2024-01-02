d3174d6ea800202e55100e5937b2398502a01217
RadosavPanic <radosav.panic01@gmail.com>
2024-01-02 19:10:05 +0100

    working with functions

---
+++ Day-7/functions.js
@@ -0,0 +1,88 @@
+// Function declaration
+
+function square() {
+  let num = 2;
+  let squareResult = num * num;
+
+  console.log(`Number squared: ${squareResult}`); // Number squared: 4
+}
+
+square();
+
+function printFullName() {
+  let firstName = "Radosav";
+  let lastName = "Panic";
+  let space = " ";
+
+  let fullName = firstName + space + lastName;
+
+  return fullName;
+}
+
+console.log(printFullName()); // Radosav Panic
+
+function areaOfCircle(radius) {
+  let area = Math.PI * radius * radius;
+
+  return area;
+}
+
+console.log(areaOfCircle(10)); // 314.1592653589793
+
+function sumArrayValues(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(sumArrayValues(numbers)); // 15
+
+// arbitrary arguments function (arguments object)
+function sumAllNums() {
+  let sum = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+
+  return sum;
+}
+
+console.log(sumAllNums(5, 20, 1, 64, 12)); // 102
+
+// Arrow function, unlimited arguments (spread operator)
+const sumNums = (...args) => {
+  let sum = 0;
+
+  for (const num of args) {
+    sum += num;
+  }
+
+  return sum;
+};
+
+console.log(sumNums(41, 30, 90, 12)); // 173
+
+// Anonymous function expression
+const cubicRes = function (number) {
+  return Math.pow(number, 3);
+};
+
+console.log(cubicRes(5)); // 125
+
+// IIFE (Immediately Invoked Function Expression) - Self Invoking Functions
+(function (n) {
+  console.log(`IIFE squared number: ${n * n}`);
+})(5); // IIFE squared number: 25
+
+let concatTwoStrings = (function (material) {
+  return "Stainless " + material;
+})("Steel");
+
+console.log(concatTwoStrings); // Stainless Steel
