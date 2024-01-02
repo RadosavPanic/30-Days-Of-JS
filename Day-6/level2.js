309cd4bd5f66c22ac3951feb89d6ab4d21783775
RadosavPanic <radosav.panic01@gmail.com>
2024-01-02 14:35:39 +0100

    completed level 2 of loops

--- Day-6/level2.js
+++ Day-6/level2.js
@@ -50,19 +50,19 @@ console.log(`Random rgb color number: ${generateRandomRgbColor()}`); // rgb(145,
 
 // Countries level
 const countriesUpper = new Array();
-for (country of countries) {
+for (const country of countries) {
   countriesUpper.push(country.toUpperCase());
 }
 console.log(countriesUpper); // ['ALBANIA', 'BOLIVIA', 'CANADA', 'DENMARK', 'ETHIOPIA', 'FINLAND', 'GERMANY', 'HUNGARY', 'IRELAND', 'JAPAN', 'KENYA']
 
 const countriesLength = Array();
-for (country of countries) {
+for (const country of countries) {
   countriesLength.push(country.length);
 }
 console.log(countriesLength); // [7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]
 
 const countriesMarkAndLength = [];
-for (country of countries) {
+for (const country of countries) {
   countriesMarkAndLength.push([
     country,
     country.slice(0, 3).toUpperCase(),
@@ -81,8 +81,96 @@ console.log(countriesMarkAndLength);
   ['Germany', 'GER', 7],
   ['Hungary', 'HUN', 7],
   ['Ireland', 'IRE', 7],
-  ['Iceland', 'ICE', 7],
   ['Japan', 'JAP', 5],
   ['Kenya', 'KEN', 5]
 ]
+*/
+
+// Check if countries contain word "land"
+const countriesIncludingLand = [];
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].includes("land")) countriesIncludingLand.push(countries[i]);
+}
+
+countriesIncludingLand.length > 0
+  ? console.log(countriesIncludingLand)
+  : console.log(`All these countries are without land`);
+// ['Finland', 'Ireland']
+
+// Check if countries end with word "ia"
+const countriesEndingWithIa = [];
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].endsWith("ia")) countriesEndingWithIa.push(countries[i]);
+}
+
+countriesEndingWithIa.length > 0
+  ? console.log(countriesEndingWithIa)
+  : console.log(`All these countries end without ia`);
+// ['Albania', 'Bolivia', 'Ethiopia']
+
+// Finding longest country length
+let longestCountry = "";
+for (const country of countries) {
+  if (longestCountry.length < country.length) longestCountry = country;
+}
+console.log(longestCountry); // Ethiopia
+
+const countriesWith5Chars = [];
+for (const country of countries) {
+  if (country.length == 5) countriesWith5Chars.push(country);
+}
+
+console.log(countriesWith5Chars); // ['Japan', 'Kenya']
+
+// webTechs level
+console.log(webTechs); // ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node', 'MongoDB']
+
+// Finding longest web tech length
+let longestWebTech = "";
+for (const tech of webTechs) {
+  if (longestWebTech.length < tech.length) longestWebTech = tech;
+}
+console.log(longestWebTech); // JavaScript
+
+const webTechsMarkAndLength = [];
+for (const tech of webTechs) {
+  webTechsMarkAndLength.push([tech, tech.length]);
+}
+console.log(webTechsMarkAndLength); // [["HTML", 4], ["CSS", 3],["JavaScript", 10],["React", 5],["Redux", 5],["Node", 4],["MongoDB", 7]]
+
+// mernStack level
+console.log(mernStack); // ['MongoDB', 'Express', 'React', 'Node']
+
+let mernStackAcronym = "";
+for (const stack of mernStack) {
+  mernStackAcronym += stack.slice(0, 1);
+}
+console.log(mernStackAcronym); // MERN
+
+// Reversing the array manually
+let fruitArray = ["banana", "orange", "mango", "lemon"];
+for (let i = 0; i < Math.floor(fruitArray.length / 2); i++) {
+  let temp = fruitArray[i];
+  fruitArray[i] = fruitArray[fruitArray.length - 1 - i];
+  fruitArray[fruitArray.length - 1 - i] = temp;
+}
+
+console.log(fruitArray); // ['lemon', 'mango', 'orange', 'banana']
+console.log(fruitArray.toReversed()); // ['banana', 'orange', 'mango', 'lemon'], new method (no mutation of original array)
+
+// fullStack level
+const fullStack = [
+  ["HTML", "CSS", "JS", "React"],
+  ["Node", "Express", "MongoDB"],
+];
+
+fullStack.flat().forEach((tech) => console.log(tech));
+/* 
+HTML
+CSS
+JS
+REACT
+NODE
+EXPRESS
+MONGODB
 */
