9f5a1a8e4b7574353620164f6aa0b192f30d4f9a
RadosavPanic <radosav.panic01@gmail.com>
2024-01-11 11:31:23 +0100

    working with higher order fns

---
+++ Day-9/higher_order_functions.js
@@ -0,0 +1,29 @@
+// Higher-order function takes function as a parameter (callback) or returns a function as a value
+
+const higherOrder = (n) => {
+  const doSomething = (m) => {
+    const doWhatever = (t) => {
+      return 2 * n + 3 * m + t;
+    };
+    return doWhatever;
+  };
+  return doSomething;
+};
+
+console.log(higherOrder(2)(3)(10)); // 23
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sumArray = (arr) => {
+  let sum = 0;
+
+  const callback = function (element) {
+    sum += element;
+  };
+
+  arr.forEach(callback);
+
+  return sum;
+};
+
+console.log(sumArray(numbers));
