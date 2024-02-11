dc106dfd868f0ccbe83a35bb3542024cee64302f
RadosavPanic <radosav.panic01@gmail.com>
2024-02-11 17:39:57 +0100

    working with functional programming fns

--- Day-9/higher_order_functions.js
+++ Day-9/higher_order_functions.js
@@ -35,3 +35,43 @@ function sayHello() {
 // setInterval(sayHello, 1000);
 
 setTimeout(sayHello, 2000);
+
+/* Functional programming concepts */
+
+// forEach
+numbers.forEach((val, i, arr) => {
+  console.log(`Value: ${val}, index: ${i}, array: ${arr}`);
+}); // Value: 1, index: 0, array: 1,2,3,4,5
+
+// map
+const modifiedNumbers = numbers.map((val) => val * 3);
+console.log(modifiedNumbers); // [3, 6, 9, 12, 15]
+
+// filter
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const countriesContainingLand = countries.filter((country) =>
+  country.includes("land")
+);
+console.log(countriesContainingLand); // ['Finland', 'Ireland']
+
+// reduce
+const sumOfNums = numbers.reduce((acc, cur) => acc + cur, 0);
+console.log(sumOfNums); // 15
+
+// every
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+const allAreStr = names.every((name) => typeof name === "string");
+console.log(allAreStr); // true
