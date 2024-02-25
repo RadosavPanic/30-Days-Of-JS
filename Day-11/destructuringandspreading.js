810ef556776dc70449c6442ede3a55053ea8b96a
RadosavPanic <radosav.panic01@gmail.com>
2024-02-25 11:42:20 +0100

    working with rest operator and spreading

--- Day-11/destructuringandspreading.js
+++ Day-11/destructuringandspreading.js
@@ -1,4 +1,4 @@
-const numbers = [1, 2, 3];
+let numbers = [1, 2, 3];
 
 const [firstNum, secondNum, thirdNum] = numbers;
 console.log(firstNum, secondNum, thirdNum); // 1 2 3
@@ -34,3 +34,30 @@ console.log(num2); // 36
 let [val1 = "John", val2, val3] = [undefined, "Brandon", "Bill"];
 console.log(val1); // John, we can use default value if array indexed value is undefined
 console.log(val2, val3); // Brandon Bill
+
+// REST operator
+numbers = [1, 2, 3, 4, 5, 6, 7];
+let [f, s, t, ...otherNums] = numbers;
+console.log(f, s, t); // 1 2 3
+console.log(otherNums); // [4, 5, 6, 7]
+
+// Spreading
+let extendedNumbers = [...numbers, 8, 9, 10];
+console.log(extendedNumbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Destructuring objects
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+};
+
+let { width, height, area, perimeter = 60 } = rectangle;
+console.log(width, height); // 20 10
+console.log(area, perimeter); // 200 60
+
+// With renaming
+let { width: w } = rectangle;
+console.log(w); // 20
+let { height: heightOfRect, area: areaOfRect } = rectangle;
+console.log(heightOfRect, areaOfRect); // 10 200
